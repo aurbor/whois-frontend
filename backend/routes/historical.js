1c9f11b76d72549c5c599135b105b2a72d0e4597
evan@icelantic.com <evan@icelantic.com>
2020-04-14 14:24:43 +0100

    Split frontend and backend

--- backend/routes/historical.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-let Lookup = require('../models/lookup.model');
-
-router.route('/').get((req, res) => {
-    Lookup.find()
-    .then(hostname => res.json(hostname))
-    .catch(err => console.error(err));
-});
